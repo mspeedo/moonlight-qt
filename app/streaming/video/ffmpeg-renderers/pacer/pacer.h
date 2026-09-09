0399b33b4bb52955b13cd65dd9eb0baaf7cfaad3
mspeedo <mspeedo@gmail.com>
2026-09-09 15:21:05 +0200

    Revert "Use latest-frame-wins render queue for VRR"
    This reverts commit d1ca4cb622d1e6d6e2e4b5ef441ccb21a424cc1e.

--- app/streaming/video/ffmpeg-renderers/pacer/pacer.h
+++ app/streaming/video/ffmpeg-renderers/pacer/pacer.h
@@ -68,7 +68,6 @@ private:
     SDL_Thread* m_VsyncThread;
     AVFrame* m_DeferredFreeFrame;
     bool m_Stopping;
-    bool m_LatestFrameWins;
 
     IVsyncSource* m_VsyncSource;
     IFFmpegRenderer* m_VsyncRenderer;
